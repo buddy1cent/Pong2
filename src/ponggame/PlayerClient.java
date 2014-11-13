bfe08eccfb03ba0b2ae1b6abbe0c10833916a75c
Buddy-1cent <Buddy-1cent@158.196.113.83>
2014-11-13 07:42:56 +0100

    new pong

---
+++ src/ponggame/PlayerClient.java
@@ -0,0 +1,71 @@
+package ponggame;
+
+import java.io.Serializable;
+
+
+public class PlayerClient implements Serializable {
+	 	
+	/**
+	 * - Player Properties -
+	 */
+	
+	private static final long serialVersionUID = 1L;
+	
+	
+	  //////////////////////////
+	 // - Client Variables - //
+	//////////////////////////
+	
+	private String name ="";
+	private int	x, y;
+    	boolean ok = false;
+    	boolean restart = false;
+    	boolean ready = false;
+        
+	public PlayerClient(String name){
+		this.name = name;
+		x = 740;
+		y = 210;
+	}
+	
+	  ///////////////////////////
+	 // - Getters & Setters - //
+	///////////////////////////
+	
+	public String getName() {
+		return name;
+	}
+        
+	public void setName(String name) {
+		this.name = name;
+	}
+        
+        public boolean isReady(){
+                return ready;
+        }
+        
+        public void setReady(){
+                ready = true;
+        }
+        
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	@Override
+	public String toString() {
+		return "PlayerClient [name=" + name + ", x=" + x + ", y=" + y + "]";
+	}
+}
